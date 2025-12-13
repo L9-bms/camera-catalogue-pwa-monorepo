08ca2881b69716dfd1a388307c906e99c771993a
callum <mail@callumwong.com>
2025-12-13 14:00:09 +1100

    starting point

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Doro } from '@ui/doro'
 import { api } from '@libs'
 
 export default async function Landing() {
@@ -6,7 +5,6 @@ export default async function Landing() {
 
     return (
         <main className="flex flex-col gap-4 justify-center items-center w-full min-h-dvh">
-            <Doro />
             <h1 className="text-2xl">{data}</h1>
         </main>
     )
