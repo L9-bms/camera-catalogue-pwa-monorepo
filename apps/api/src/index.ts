b18915152aad04ccf6519fbdf0b8d2c4aa51bd59
callum <mail@callumwong.com>
2025-12-15 22:17:23 +1100

    convert db queries to raw sql, blergh

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,25 @@
 import { Elysia, t } from 'elysia'
 import { cors } from '@elysiajs/cors'
-import { eq, ilike, or, and, SQL, sql } from 'drizzle-orm'
+import { sql } from 'drizzle-orm'
 
 import { otel, oapi } from '@api/modules'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { cameraTable, brandTable, sensorTable } from './database/schema'
 
 const db = drizzle(process.env.DATABASE_URL!)
+
+type FullCameraSelectType = Omit<
+    typeof cameraTable.$inferSelect,
+    'brand_id' | 'sensor_id'
+> & {
+    brand: string | null
+    sensor: string | null
+}
+
+type PartialCameraSelectType = Pick<
+    typeof cameraTable.$inferSelect,
+    'id' | 'name'
+>
 
 export const app = new Elysia()
     .use(oapi)
@@ -20,30 +33,25 @@ export const app = new Elysia()
     .get(
         '/cameras',
         async ({ query }) => {
-            const filters: SQL[] = []
-            if (query.brand) filters.push(eq(cameraTable.brand_id, query.brand))
-            if (query.sensor)
-                filters.push(eq(cameraTable.sensor_id, query.sensor))
+            // this is solely to meet the requirements of the task, which includes writing SQL
 
-            return await db
-                .select({
-                    id: cameraTable.id,
-                    name: cameraTable.name,
-                    brand: brandTable.name,
-                    sensor: sensorTable.name,
-                    price: cameraTable.price,
-                    megapixels: cameraTable.megapixels,
-                    image: cameraTable.image
-                })
-                .from(cameraTable)
-                .leftJoin(brandTable, eq(cameraTable.brand_id, brandTable.id))
-                .leftJoin(
-                    sensorTable,
-                    eq(cameraTable.sensor_id, sensorTable.id)
-                )
-                .where(and(...filters))
-                // sql injection? who's sql and why are they being injected?
-                .orderBy(sql.raw(`"camera".${query.sortBy} ${query.sortOrder}`))
+            const selectSql = sql`
+                SELECT camera.id, camera.name, brand.name AS brand, sensor.name AS sensor, camera.price, camera.megapixels, camera.image
+                FROM camera
+                LEFT JOIN brand ON camera.brand_id = brand.id
+                LEFT JOIN sensor ON camera.sensor_id = sensor.id
+            `
+
+            if (query.brand)
+                selectSql.append(sql` WHERE camera.brand_id = ${query.brand}`)
+            if (query.sensor)
+                selectSql.append(sql` WHERE camera.sensor_id = ${query.sensor}`)
+
+            selectSql.append(
+                sql.raw(` ORDER BY camera.${query.sortBy} ${query.sortOrder};`)
+            )
+
+            return (await db.execute(selectSql)).rows as FullCameraSelectType[]
         },
         {
             query: t.Object({
@@ -70,19 +78,15 @@ export const app = new Elysia()
         async ({ query }) => {
             const searchPattern = `%${query.q.trim()}%`
 
-            return await db
-                .select({
-                    id: cameraTable.id,
-                    name: cameraTable.name
-                })
-                .from(cameraTable)
-                .where(
-                    or(
-                        ilike(cameraTable.name, searchPattern),
-                        ilike(cameraTable.brand_id, searchPattern)
-                    )
-                )
-                .limit(10)
+            return (
+                await db.execute(sql`
+                    SELECT id, name
+                    FROM camera
+                    WHERE camera.name ILIKE ${searchPattern}
+                        OR camera.brand_id ILIKE ${searchPattern}
+                    LIMIT 10;
+                `)
+            ).rows as PartialCameraSelectType[]
         },
         {
             query: t.Object({
@@ -91,48 +95,40 @@ export const app = new Elysia()
         }
     )
     .get('/cameras/brands', async () => {
-        const brands = await db
-            .select({ id: brandTable.id, name: brandTable.name })
-            .from(brandTable)
-            .orderBy(brandTable.name)
-
-        return brands
+        return (
+            await db.execute(sql`
+                SELECT *
+                FROM brand
+                ORDER BY name
+            `)
+        ).rows as (typeof brandTable.$inferSelect)[]
     })
     .get('/cameras/sensors', async () => {
-        const sensors = await db
-            .select({ id: sensorTable.id, name: sensorTable.name })
-            .from(sensorTable)
-            .orderBy(sensorTable.name)
-
-        return sensors
+        return (
+            await db.execute(sql`
+                SELECT *
+                FROM sensor
+                ORDER BY name
+            `)
+        ).rows as (typeof sensorTable.$inferSelect)[]
     })
     .get(
         '/cameras/:id',
         async ({ params }) => {
-            const camera = await db
-                .select({
-                    id: cameraTable.id,
-                    name: cameraTable.name,
-                    brand: brandTable.name,
-                    sensor: sensorTable.name,
-                    price: cameraTable.price,
-                    megapixels: cameraTable.megapixels,
-                    image: cameraTable.image
-                })
-                .from(cameraTable)
-                .leftJoin(brandTable, eq(cameraTable.brand_id, brandTable.id))
-                .leftJoin(
-                    sensorTable,
-                    eq(cameraTable.sensor_id, sensorTable.id)
-                )
-                .where(eq(cameraTable.id, params.id))
-                .limit(1)
+            const result = await db.execute(sql`
+                SELECT camera.id, camera.name, brand.name AS brand, sensor.name AS sensor, camera.price, camera.megapixels, camera.image
+                FROM camera
+                LEFT JOIN brand ON camera.brand_id = brand.id
+                LEFT JOIN sensor ON camera.sensor_id = sensor.id
+                WHERE camera.id = ${params.id}
+                LIMIT 1
+            `)
 
-            if (camera.length === 0) {
+            if (result.rows.length === 0) {
                 throw new Error('not found')
             }
 
-            return camera[0]
+            return result.rows[0]
         },
         {
             params: t.Object({
